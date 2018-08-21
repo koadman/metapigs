ba5324cd6cb3a248cafbc6f530859dac24fbff91
Aaron Darling <aaron.darling@uts.edu.au>
2018-08-21 11:04:19 +1000

    some fine tuning & syntax fixes

--- scripts/robot_pooling.py
+++ scripts/robot_pooling.py
@@ -3,7 +3,14 @@
 import numpy as np
 import sys
 import re
-from opentrons import containers, instruments
+#from opentrons import containers, instruments
+
+# alter the ratio of the following samples:
+custom={
+    "7D9":10,
+    "8G12":10,
+    "5A2":10
+}
 
 # configure the unused well blacklist
 blacklist={}
@@ -40,6 +47,8 @@ for s_row in range(count_table.shape[0]):
     col = rrr.group(3)
     code = str(plate)+row+str(col)
     well_counts[code]=max(rc_floor,int(count_table[s_row,1]))
+    if code in custom:
+        well_counts[code] /= custom[code]
 
 # add zero counts for any missing wells
 for plate in range(1,10):
@@ -57,7 +66,10 @@ pool_minmax = {cur_pool:[rc_floor,rc_floor]}
 max_relrange = 1.1
 pool_samples = {0:[]}
 for count, well in sorted((value, key) for (key,value) in well_counts.items()):
-    if pool_sample_count[cur_pool] >= samples_per_pool or (cur_pool > 5 and pool_minmax[cur_pool][0]*max_relrange < count):
+    if ( pool_sample_count[cur_pool] >= samples_per_pool or
+        (cur_pool > 5 and pool_minmax[cur_pool][0]*max_relrange < count) or
+        (pool_minmax[cur_pool][0] < 140 and count > 140) or
+        (pool_minmax[cur_pool][0] < 250 and count > 250)):
         # start a new pool
         cur_pool += 1
         pool_sample_count[cur_pool] = 0
@@ -104,39 +116,48 @@ lib_plates = {
 	8:containers.load('96-PCR-tall', 'D1', 'lib_plate_8'),
 	9:containers.load('96-PCR-tall', 'D2', 'lib_plate_9'),
 	10:containers.load('96-PCR-tall', 'D3', 'lib_plate_10')
-	}
+}
 
 pool_plate = containers.load('96-PCR-flat', 'C2', 'pool_plate')
 trash = containers.load('trash-box', 'E1', 'trash')
 
 cur_row=1
-row_letters={1:'A',2:'B',3:'C',4:'D'}
+row_letters={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H'}
 cur_col=1
+tip_col=1
+tip_row=1
 for pool in pool_samples:
-	pool_from = []
-	for sample in pool_samples[pool]:
-		rrr = re.search('(\d+)(\w\d+)',sample)
-		plate = rrr.group(1)
-		well = rrr.group(2)
-		pool_from.append( lib_plates[ plate ].well(well).bottom() )
-	pool_dest_well = row_letters[cur_row]+str(cur_col)
-	pool_dest = pool_plate.well(pool_dest_well).bottom()
-	cur_col += 1
-	if cur_col > 12:
-		cur_col = 1
-		cur_row += 1
+    pool_from = []
+    for sample in pool_samples[pool]:
+        rrr = re.search('(\d+)(\w\d+)',sample)
+        plate = rrr.group(1)
+        well = rrr.group(2)
+        pool_from.append( lib_plates[ plate ].well(well).bottom() )
+    pool_dest_well = row_letters[cur_row]+str(cur_col)
+    pool_dest = pool_plate.well(pool_dest_well).bottom()
+    cur_col += 1
+    if cur_col > 12:
+        cur_col = 1
+        cur_row += 1
+
     #pick up new tip first
-    pipette.pick_up_tip(p10rack.wells('A1'))
-	p10.transfer(
-		10,
-		pool_from,
-		pool_dest,
-		disposal_vol=0,
-		mix_before=(3), # mix 2 times
-	    mix_after=(3),  # mix 3 times
-		touch_tip=True,
-		blow_out=True,
-		new_tip='never')
+    tip_well = row_letters[tip_row]+str(tip_col)
+    tip_col += 1
+    if tip_col > 12:
+        tip_col = 1
+        tip_row += 1
+
+    pipette.pick_up_tip(p10rack.wells(tip_well))
+    p10.transfer(
+        10,
+        pool_from,
+        pool_dest,
+        disposal_vol=0,
+        mix_before=(3), # mix 3 times
+        mix_after=(3),  # mix 3 times
+        touch_tip=True,
+        blow_out=True,
+        new_tip='never')
 
     pipette.drop_tip(trash)
     robot.home()
