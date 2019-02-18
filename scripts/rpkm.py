fa340740e1fcef9bee6d4fec36c5e95fe7ae1122
Matthew DeMaere <matt.demaere@gmail.com>
2019-02-18 15:56:35 +1100

    Reformat to PEP standard

--- scripts/rpkm.py
+++ scripts/rpkm.py
@@ -17,31 +17,31 @@ parser.add_argument('sample_id', type=str, nargs='+',
                     help='Name to tag results with in tabular output')
 args = parser.parse_args()
 
-query_file = open(args.query_file,'r')
-m8file = open(args.m8_file,'r')
+query_file = open(args.query_file, 'r')
+m8file = open(args.m8_file, 'r')
 eval_threshold = args.evalue_threshold
 fq_file = gzip.open(args.fastq_file)
 sample_id = args.sample_id
 
-fq_lines=0
+fq_lines = 0
 for line in fq_file:
-	fq_lines += 1
+    fq_lines += 1
 
 prev_q = ""
-gene_hits={}
-gene_kbp={}
+gene_hits = {}
+gene_kbp = {}
 for line in m8file:
-	d = line.rstrip().split('\t')
-	if d[0]==prev_q:
-		continue	# we've already seen this query and it can only be used once
-	if float(d[10])>eval_threshold:
-		continue	# hit not strong enough
-	if not d[1] in gene_hits:
-		gene_hits[d[1]]=0	# hit a new DB sequence. initialize to 0
-		gene_kbp[d[1]]=1.1 # HACK - this needs to get read in from the DB
-	gene_hits[d[1]]+= 1.0/gene_kbp[d[1]]
+    d = line.rstrip().split('\t')
+    if d[0] == prev_q:
+        continue  # we've already seen this query and it can only be used once
+    if float(d[10]) > eval_threshold:
+        continue  # hit not strong enough
+    if not d[1] in gene_hits:
+        gene_hits[d[1]] = 0  # hit a new DB sequence. initialize to 0
+        gene_kbp[d[1]] = 1.1  # HACK - this needs to get read in from the DB
+    gene_hits[d[1]] += 1.0 / gene_kbp[d[1]]
 
-millions = fq_lines/4000000
+millions = fq_lines / 4000000
 
 for gene in gene_hits:
-	print(sample_id+'\t'+gene+'\t'+str(gene_hits[gene]/millions))
+    print(sample_id + '\t' + gene + '\t' + str(gene_hits[gene] / millions))
