b9bf5cf1f3c2605e769336a5779230c43b4c296d
Aaron Darling <aaron.darling@uts.edu.au>
2019-02-18 16:50:55 +1100

    [AD] add parsing of gene lengths

--- scripts/rpkm.py
+++ scripts/rpkm.py
@@ -3,17 +3,18 @@ import os
 import sys
 import gzip
 import argparse
+from Bio import SeqIO
 
 parser = argparse.ArgumentParser(description='Process a BLAST m8 file to rpkm')
-parser.add_argument('--db_file', type=str,
+parser.add_argument('--db-file', type=str,
                     help='DB sequence file')
-parser.add_argument('--m8_file', type=str,
+parser.add_argument('--m8-file', type=str,
                     help='DB hits in m8 format')
-parser.add_argument('--evalue_threshold', type=float,
+parser.add_argument('--evalue-threshold', type=float,
                     help='significance threshold for inclusion of hit')
-parser.add_argument('--fastq_file', type=str,
+parser.add_argument('--fastq-file', type=str,
                     help='Sequence reads FastQ file')
-parser.add_argument('--sample_id', type=str,
+parser.add_argument('--sample-id', type=str,
                     help='Name to tag results with in tabular output')
 args = parser.parse_args()
 
@@ -23,13 +24,17 @@ eval_threshold = args.evalue_threshold
 fq_file = gzip.open(args.fastq_file)
 sample_id = args.sample_id
 
+
+gene_kbp = {}
+for record in SeqIO.parse(args.db_file, "fasta"):
+    gene_kbp[record.id]=len(record)/1000
+
 fq_lines = 0
 for line in fq_file:
     fq_lines += 1
 
 prev_q = ""
 gene_hits = {}
-gene_kbp = {}
 for line in m8file:
     d = line.rstrip().split('\t')
     if d[0] == prev_q:
@@ -38,7 +43,6 @@ for line in m8file:
         continue  # hit not strong enough
     if not d[1] in gene_hits:
         gene_hits[d[1]] = 0  # hit a new DB sequence. initialize to 0
-        gene_kbp[d[1]] = 1.1  # HACK - this needs to get read in from the DB
     gene_hits[d[1]] += 1.0 / gene_kbp[d[1]]
 
 millions = fq_lines / 4000000
