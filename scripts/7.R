3758e47d9b130e362d3ed5031ae23c8ff4ccf1ed
Daniela Gaio <madonnazzzzz.dg@gmail.com>
2019-10-09 18:15:56 +1100

    plots bins abundance change that belong to the same cluster

--- scripts/7.R
+++ scripts/7.R
@@ -4,11 +4,9 @@
 
 library(readxl)
 
+#input files
 cohorts <- read_excel("~/metapigs/source_data/cohorts.xlsx")
-View(cohorts)
-
 df_total <- read.csv("~/Desktop/bins_clustering_parsing_dataframes/merged_all_clustered_wa_bins.csv")
-View(df_total)
 
 colnames(df_total)[colnames(df_total)=="X170130.01.bam"] <- "17-01-30.1"
 colnames(df_total)[colnames(df_total)=="X170131.01.bam"] <- "17-01-31.1"
@@ -46,23 +44,19 @@ library(data.table)
 
 #merge cohorts
 df_total_2 <- merge.data.frame(df_total, cohorts, by.x="pig", by.y = "Animal ID")
-View(df_total_2)
 
 #move Cohort Name column to first position of dataframe
 df_total_3 <- df_total_2 %>% 
   select("Cohort Name", everything())
 #rename to eliminate space
 colnames(df_total_3)[colnames(df_total_3)=="Cohort Name"] <- "cohort"
-View(df_total_3)
 
 #fill out column "secondary_cluster" -empty cells with NA
 library(dplyr)
 df_total_4 <- mutate_all(df_total_3, funs(na_if(.,"")))
-View(df_total_4)
 
 #subset to contain only rows that have a secondary_cluster value (not na)
 clustered_only<-subset(df_total_4, (!is.na(df_total_4[,4])))
-View(clustered_only)
 
 
 #subset based on number of occurrences of cluster IDs. 
@@ -74,7 +68,6 @@ newdata <- vector()
 for( k in z ) {
   newdata <- rbind(newdata,k)
 }
-View(newdata)
 
 #these are to test if the subsetting to x number of occurrences is working fine
 #change the secondary_cluster ID to check which ones are broughtforward in the analysis
@@ -94,7 +87,6 @@ View(newdata)
 #make long (one value per row)
 library(reshape)
 newdata_long <- melt(newdata, id=c("cohort", "pig", "bin", "secondary_cluster"))
-View(newdata_long)
 
 #split column "variable" using dot separator
 install.packages("splitstackshape")
@@ -103,30 +95,22 @@ NL2 <- cSplit(newdata_long, "variable", ".")
 # rename new columns
 colnames(NL2)[colnames(NL2)=="variable_1"] <- "date"
 colnames(NL2)[colnames(NL2)=="variable_2"] <- "replicate"
-View(NL2)
 
 #reformat to be recognized as dates
 NL2$date <- as.Date(NL2$date, format = "%y-%m-%d")
 NL2$date <- format(NL2$date, "%y-%m-%d")
-View(NL2)
 
 #keep only rows that have values (exclude NA rows)
 all_but_NA <- NL2[complete.cases(NL2), ]
-View(all_but_NA)
 
 #return rows that have replicate==1, ==2, ==3, ==4
 NLrep1 <- all_but_NA[which(all_but_NA$replicate == 1), ]
 NLrep2 <- all_but_NA[which(all_but_NA$replicate == 2), ]
 NLrep3 <- all_but_NA[which(all_but_NA$replicate == 3), ]
 NLrep4 <- all_but_NA[which(all_but_NA$replicate == 4), ]
-View(NLrep1)
-View(NLrep2)
-View(NLrep3)
-View(NLrep4)
 
 #concatenate the above dataframes
 total <- rbind(NLrep1, NLrep2, NLrep3, NLrep4)
-View(total)
 
 #take mean out of the replicates (works right)
 library(dplyr)
@@ -134,12 +118,11 @@ total_dereplicated <- total %>% group_by(pig, bin, secondary_cluster, cohort, da
   summarize(value.mean = mean(value))
 View(total_dereplicated)
 
-#you may need to change dates to actual dates because otherwise it may be plotted not in the right order
-
-
-
 #write out to play with it interactively
 fwrite(x = total_dereplicated, file = "~/Desktop/bins_clustering_parsing_dataframes/total_dereplicated.csv")
+
+
+
 
 #let's try to plot one of them: 
 #for uniq secondary_cluster ID, make a plot: 
@@ -154,6 +137,27 @@ gg_fun <- function(parameter, dt){
     ggtitle(parameter)
   return(p)
 }
+
+plot_list <- lapply(unique(total_dereplicated$secondary_cluster), gg_fun, dt = total_dereplicated)
+#save all plots into one pdf
+pdf("~/Desktop/bins_clustering_parsing_dataframes/plots_by_secondary_cluster.pdf")
+plot_list
+dev.off()
+
+
+
+#plot multiple plots (1 per cohort) in each page (1 page : 1 cluster) IT WORKS! 
+library(ggplot2)
+gg_fun <- function(parameter, dt){
+  p <- ggplot(dt[dt$secondary_cluster == parameter, ], aes(x=date, y=value.mean, colour = factor(cohort)))+
+    geom_point() + facet_wrap(~ cohort, ncol = 2, scales = "free") +
+    guides(colour = "none") +
+    #geom_line to connect the dots by pig
+    geom_line(aes(group = pig)) + 
+    theme(text = element_text(size=5), axis.text.x = element_text(angle = 45, hjust=1)) +
+    ggtitle(parameter)
+  return(p)
+}
 plot_list <- lapply(unique(total_dereplicated$secondary_cluster), gg_fun, dt = total_dereplicated)
 #save all plots into one pdf
 pdf("~/Desktop/bins_clustering_parsing_dataframes/plots_by_secondary_cluster.pdf")
@@ -163,19 +167,15 @@ dev.off()
 
 
 
+#TO DOs improvements: 
+# y axis limit make it depend on secondary_cluster max value per secondary_cluster
+#reorder cohorts plots appeareance 
+#leave mothers out
 
 
 
 
 
-
-
-#improvement: 
-#plot each factor(cohort) in separate plots in the same page 
-for (var in unique(total_dereplicated$cohort)) {
-  dev.new()
-  print( ggplot(mydata[mydata$Variety==var,], aes(Var1, Var2)) + geom_point() )
-}
 
 
 
